1ee4714028d0fffbcef12206bc4cc274d8495c47
AnastaSAV <anastasia.personalbox@gmail.com>
2022-02-16 12:50:37 +0200

    added files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # web-server
+Practice GET && POST requests
---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const port = process.env.PORT || 8090;
+
+app.use(cors());
+app.get('/', (req, res) => {
+	res.send(__dirname + '/public');
+});
+
+app.post('/public', (req, res) => {
+	res.json([]);
+});
+app.get('/', (req, res) => {
+	res.send({message: 'hello www'})});
+	
+	let users = [
+		{
+			id: 1,
+			name: 'Bohdan',
+			description: 'Software engeneer',
+		},
+		{
+			id: 2,
+			name: 'Vlad',
+			description: 'Full stack dev',
+		},
+		{
+			id: 3,
+			name: 'Anna',
+			description: 'Technical writer',
+		},
+		{
+			id: 4,
+			name: 'Anastasiia',
+			description: 'JS dev',
+		},
+		{
+			id: 5,
+			name: 'Ivan',
+			description: 'Dev Ops',
+		},
+		{
+			id: 6,
+			name: 'Evgenii',
+			description: 'Solution architect',
+		},
+		{
+			id: 7,
+			name: 'Mariia',
+			description: 'Graphic designer',
+		},
+		{
+			id: 8,
+			name: 'Nikolay',
+			description: 'CTO',
+		},
+	]
+	
+	app.get('/users', (req, res) => {
+		res.json(users);
+	});
+	app.delete('/users/:id', (req, res) => {
+		users = users.filter(item => item.id !== id);
+		res.json(users);
+	});
+	
+	server.listen(port, () => {
+		console.log('listening on *:${port}');
+	});
