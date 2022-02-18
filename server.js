b3775954a08bbc921652178784322a9c60a2d3f0
AnastaSAV <anastasia.personalbox@gmail.com>
2022-02-18 14:08:53 +0200

    fixed description

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/users', (req, res) => {
 	res.json(users);
 });
 app.post('/users', (req, res) => {
-	users.push({id: req.body?.id, name: req.body?.name, price: description.body?.description});
+	users.push({id: req.body?.id, name: req.body?.name, description: req.body?.description});
 	res.json(users);
 });
 app.delete('/users/:id', (req, res) => {
